9184c2f772b9d62cc35f0be989d018ed1563b488
Austin Keller <austin474@gmail.com>
2020-10-19 02:50:35 -0700

    Refactor to comply with PEP using black and flake8

--- tests/test_coffeegrindsize.py
+++ tests/test_coffeegrindsize.py
@@ -1,7 +1,8 @@
 from click.testing import CliRunner
 from coffeegrindsize.scripts.coffeegrindsize import main
 
+
 def test_main():
-  runner = CliRunner()
-  result = runner.invoke(main, ['--help'])
-  assert result.exit_code == 0
+    runner = CliRunner()
+    result = runner.invoke(main, ["--help"])
+    assert result.exit_code == 0
